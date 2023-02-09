8f33f979618e5a2a8c867739a746f866f576db7c
Klein254 <rayobiero62@gmail.com>
2023-02-09 09:15:43 +0300

    data structures

---
+++ dictionaries.py
@@ -0,0 +1,7 @@
+#name="Francis"
+#print(name)
+
+
+friends = {"louis:32, ashley;23,kyle:26,Quincey;Rita:24"}
+
+print(friends)
---
+++ ifstatements.py
@@ -0,0 +1,11 @@
+dial = int(input("Enter a number"))
+if dial == 1 :
+    print("data deals")
+elif dial == 2 :
+    print("Daily bunldes")
+elif dial == 3:
+    print("weekly bundles")
+elif dial ==4 :
+    print("monthly bunldes")
+else:
+    print("Okoa jahazi")
